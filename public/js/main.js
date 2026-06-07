0f3393f718fcdf0b7fa3273b7bf6c5ac0d97c1c1
KittoAres <103236412+KitoAres@users.noreply.github.com>
2026-06-07 00:26:35 -0400

    Add files via upload

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('OMAKASE Inicializado correctamente.');
+    
+    // Aquí puedes integrar la lógica de Supabase
+    // const supabase = createClient('TU_URL', 'TU_KEY');
+});

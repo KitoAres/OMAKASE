7544ba32185745226bab4f023cb0068e914b5318
KittoAres <103236412+KitoAres@users.noreply.github.com>
2026-06-07 00:50:49 -0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,4 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('OMAKASE Inicializado correctamente.');
     
-    // Aquí puedes integrar la lógica de Supabase
-    // const supabase = createClient('TU_URL', 'TU_KEY');
 });
